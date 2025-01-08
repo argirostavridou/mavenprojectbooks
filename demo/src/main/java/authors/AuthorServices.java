bda51376a6863c6e0e48022df3711d2e80636ed0
argirostavridou <stavridarg@gmail.com>
2025-01-08 12:11:25 +0200

    methods added

--- demo/src/main/java/authors/AuthorServices.java
+++ demo/src/main/java/authors/AuthorServices.java
@@ -10,7 +10,7 @@ public class AuthorServices {
 	private List<Author> authors = new ArrayList<Author>();
 	
 	// Prepei na kanoume methodous gia leitourgikothtes prosthiki/diagrafi stin lista
-	//adds a new theme to the list themes
+	//adds a new author to the list authors
 		public void addAuthor(Author author) {
 			if (author != null) {
 			     authors.add(author);
@@ -21,4 +21,23 @@ public class AuthorServices {
 			}
 			
 		}
+		
+		// removes a author from the list if it already exists
+		public void removeAuthor(Author author) {
+			if (authors.contains(author)) {
+				authors.remove(author);
+				System.out.println("Author removed:" + author ); }
+				else {
+					System.out.println("Author was not found in the list.");
+				}
+			}
+		// methodos gia na emfanizei thn lista
+		public void printAuthorss() {
+			System.out.println("Authors in the list:");
+			for (Author author : authors) {
+				System.out.println(author);
+			}
+			}
+		
+		// methodos pou na kanei update
 }
--- demo/src/main/java/books/BookServices.java
+++ demo/src/main/java/books/BookServices.java
@@ -2,6 +2,8 @@ package books;
 import java.util.ArrayList;
 import java.util.List;
 
+import themes.Theme;
+
 
 
 
@@ -18,5 +20,24 @@ public class BookServices {
 		else {
 			System.out.println("Can not add null book");
 		}
+   }
+	    // removes a book from the list if it already exists
+		public void removeBook(Book book) {
+			if (books.contains(book)) {
+				books.remove(book);
+				System.out.println("Book removed:" + book ); }
+				else {
+					System.out.println("Book was not found in the list.");
+				}
+			}
+		// methodos gia na emfanizei thn lista
+		public void printBooks() {
+			System.out.println("Books in the list:");
+			for (Book book : books) {
+				System.out.println(book);
+			}
+			}
+		
+		// methodos pou na kanei update
+	
 }
-	}
