2eeeb2c6fcbcfcb63da8d8b91764e1344d8f4cdd
argirostavridou <stavridarg@gmail.com>
2025-01-08 12:03:36 +0200

    method added

--- demo/src/main/java/authors/AuthorServices.java
+++ demo/src/main/java/authors/AuthorServices.java
@@ -7,7 +7,18 @@ import themes.Theme;
 
 public class AuthorServices {
 	
-	private List<Author> themes = new ArrayList<Author>();
+	private List<Author> authors = new ArrayList<Author>();
 	
 	// Prepei na kanoume methodous gia leitourgikothtes prosthiki/diagrafi stin lista
+	//adds a new theme to the list themes
+		public void addAuthor(Author author) {
+			if (author != null) {
+			     authors.add(author);
+			     System.out.println("Author added" + author);
+			}
+			else {
+				System.out.println("Can not add null author");
+			}
+			
+		}
 }
