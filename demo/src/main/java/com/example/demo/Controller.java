60677a8648e02e6ac9623d08e4f35261762390b2
Theodore Maikantis <teomaik19@gmail.com>
2025-01-09 14:11:49 +0200

    test

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,12 +1,13 @@
-package Controllers;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
-public class Comtroller {
+@RequestMapping("themes")
+public class Controller {
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello from Theme controller";
