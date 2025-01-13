aaab70738f8c8d2ccc8a700f851685fbf64daf02
argirostavridou <stavridarg@gmail.com>
2025-01-13 09:48:17 +0200

    repositoris

--- demo/src/main/java/com/example/demo/all/ThemeRepository2.java
+++ demo/src/main/java/com/example/demo/all/ThemeRepository2.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ThemeRepository extends JpaRepository<Theme, Integer>{
+public interface ThemeRepository2 extends JpaRepository<Theme, Integer> {
+
+	Theme findByName(String name);
 
 }
